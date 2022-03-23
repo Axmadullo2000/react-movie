6d6cb9c1ff7451fb9bd5e9da811b70582dd17e5c
Axmadullo2000 <axmadullo2000@gmail.com>
2022-03-23 21:47:25 +0500

    New Changes

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -4,7 +4,6 @@ export default function Header() {
             <div className="nav-wrapper">
             <a href className="brand-logo center">Logo</a>
             <ul id="nav-mobile" className="left hide-on-med-and-down">
-                <li><a href>All</a></li>
                 <li><a href>Movies</a></li>
                 <li><a href>Cartoons</a></li>
             </ul>
